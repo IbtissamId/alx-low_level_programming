07ffa57c813b6e9e6e1b55405327039b633f9630
IDIR IBTISSAM <ibtissam.id98@gmail.com>
2023-03-22 15:31:30 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,27 @@
-#include"main.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * print_to_98 - print n to 98 counts
- *           separated by comma, followed
- *           by space and number should be
- *           printed in order
- *
- * @n: input
-*/
+ * print_to_98 - print numbers n to 98
+ * @n: integer argument
+ */
 
 void print_to_98(int n)
 {
-	int counter;
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
 
-	if (n > 98)
-		for (counter = n; counter > 98; --counter)
-			printf("%d, ", counter);
-	else
-		for (counter = n; counter < 98; ++counter)
-			printf("%d, ", counter);
-	printf("98\n");
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+
+	if (n == 98)
+		printf("%d", n);
+
+	printf("\n");
 }
