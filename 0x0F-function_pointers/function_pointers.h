e11200d88918440fe61a18c3be6ffe2f5d1f677f
IDIR IBTISSAM <ibtissam.id98@gmail.com>
2023-04-19 17:00:48 -0700

    print name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef Function_Pointers_H
+#define Function_Pointers_H
+#include <stddef.h>
+#include <stdlib.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
