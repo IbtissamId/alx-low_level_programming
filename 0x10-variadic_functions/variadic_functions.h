3bb43bf1da8aac1a55b9a3142816d465cf841e81
IDIR IBTISSAM <ibtissam.id98@gmail.com>
2023-04-20 18:23:07 -0700

    test

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _Variadic_H
 #define _Variadic_H
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
