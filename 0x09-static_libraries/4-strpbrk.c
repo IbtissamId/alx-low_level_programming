bebfe8b4abace69929932185527e5224190ebc8f
IbtissamId <ibtissam.id98@gmail.com>
2023-06-12 14:32:58 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,1 +1,11 @@
-char *_strpbrk(char *s, char *accept);
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ * @s:first string search
+ * @accept: matches
+ * Return: a pointer to the byte
+ */
+char *_strpbrk(char *s, char *accept) {}
--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,1 +1,11 @@
-char *_strstr(char *haystack, char *needle);
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - locates substring
+ * @haystack: longer string search
+ * @needle: the first occurrence of the substring
+ * Return: a pointer beg of substring or @Null if it not foound.
+ */
+
+char *_strstr(char *haystack, char *needle) {}
