eab9c1e0045830463b9fdce4852f9a18b62fb529
IbtissamId <ibtissam.id98@gmail.com>
2023-06-15 15:52:50 -0700

    allocate

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+
+/**
+ * malloc_checked - a function that allocates memory
+ * @b: byte to allocate
+ * Return: a function that allocates memory
+ */
+void *malloc_checked(unsigned int b)
+{
+        void *p;
+        p = malloc(b);
+
+
+        if (p == NULL)
+                exit(98);
+        return (p);
+}
